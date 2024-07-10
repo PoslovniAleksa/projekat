f8acc08f11bd30b418ac95295bcd821c4947c850
Filip Urosevic <f_urosevic@wargaming.net>
2024-07-10 13:02:16 +0200

    added frontend skeleton.

---
+++ frontend/index.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+		document.getElementById("content").innerHTML += "News info 1.";
+		document.getElementById("content").innerHTML += "<br>";
+		document.getElementById("content").innerHTML += "News info 2.";
+});
+

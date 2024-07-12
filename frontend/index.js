a31bc846810c8d3133d3e1e03df283d78bc4e555
Filip Urosevic <f_urosevic@wargaming.net>
2024-07-12 10:49:12 +0200

    frontend skeleton completed

--- frontend/index.js
+++ frontend/index.js
@@ -2,33 +2,48 @@ document.addEventListener("DOMContentLoaded", function() {
 		
 	let text = '{"article" : "good article"}';
 	let obj = JSON.parse(text);
-		for (let i = 0; i < 30; i++) {
-			var entry = document.createElement('li');
-			
-			var title = document.createElement('div');
-				var titleText = document.createElement('span');
-				titleText.className = "titleText";
-				titleText.appendChild(document.createTextNode(obj.article + " ."));
-				titleText.setAttribute("link", (i+1) + obj.article);
-				title.appendChild(titleText);
-				
-				titleText.addEventListener('click', (function(currentI) {
-					return function() {
-						alert(this.getAttribute("link"));
-					};
-				})(i));
-			entry.appendChild(title);
-			
-			var info = document.createElement('div');
-				var infoText = document.createElement('span');
-				infoText.appendChild(document.createTextNode("info"));
-				infoText.style.color = "gray";
-				infoText.style.fontSize = "12px";
-
-			info.appendChild(infoText);
-			entry.appendChild(info);
-			
-			
-			document.getElementById("content").appendChild(entry);
+	loadContent(obj);
+	
+	var search_bar = document.getElementById("search");
+	search_bar.addEventListener('keydown', function(event) {
+		if (event.key === 'Enter') {
+			alert(search_bar.value);
 		}
+	});
 });
+
+function loadContent(obj) {
+	for (let i = 0; i < 30; i++) {
+		var entry = document.createElement('li');
+		
+		var title = document.createElement('div');
+			var titleText = document.createElement('span');
+			titleText.className = "titleText";
+			titleText.appendChild(document.createTextNode(obj.article + " ."));
+			titleText.setAttribute("link", (i+1) + obj.article);
+			title.appendChild(titleText);
+			
+			titleText.addEventListener('click', (function(currentI) {
+				return function() {
+					alert(this.getAttribute("link"));
+				};
+			})(i));
+		entry.appendChild(title);
+		
+		var info = document.createElement('div');
+			var infoText = document.createElement('span');
+			infoText.appendChild(document.createTextNode("info"));
+			infoText.style.color = "gray";
+			infoText.style.fontSize = "12px";
+
+		info.appendChild(infoText);
+		entry.appendChild(info);
+		
+		document.getElementById("content").appendChild(entry);
+	
+	}
+}
+
+function loadNext () {
+	alert("works");
+}
